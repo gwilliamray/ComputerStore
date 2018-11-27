2fd9a3f0d0b0582e8f7b21806ff319cbc34ca22f
Glenn Ray <gray@southeast.edu>
2018-11-27 10:20:54 -0600

    Added Part Model

---
+++ CompStore/Models/Part.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CompStore.Models
+{
+    public class Part
+    {
+        public int Id { get; set; }
+        public string PartNumber { get; set; }
+        public string  Description { get; set; }
+        public decimal Cost { get; set; }
+        public decimal ListPrice { get; set; }
+
+
+    }
+}
